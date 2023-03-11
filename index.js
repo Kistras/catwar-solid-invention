8eca0ae349c76699a3d60e75fb7e0df594c75ddc
Kistras <81854722+Kistras@users.noreply.github.com>
2023-03-11 21:07:22 +0500

    Блоги, проверка на спам, время работы Константы теперь в начале кода Куча рандомной фигни

--- index.js
+++ index.js
@@ -6,29 +6,48 @@ A light that stole the dark from me
 But did the dark deserve to be
 */
 const FIREFOX_PATH = 'C:/Program Files/Mozilla Firefox/firefox.exe'
-EMAIL = "tnay1mi4sye@gmail.com"
-PASSWORD = "throwaway666"
+const EMAIL = "tnay1mi4sye@gmail.com"
+const PASSWORD = "throwaway666"
+const POSTTHR = 2 // Кол-во постов сверху, среди которых должен быть наш
 
-// TODO 
-postedid = 0
-blogid = 0
-postthr = 2 // Кол-во постов
-//text = "[center][img]http://d.zaix.ru/y7Sz.png[/img][/center][size=13][center]Достижение за 5 кролей![/center][/size]"
-text = "[img]https://www.meme-arsenal.com/memes/ce0b5f8bd8c15fafb1d4fc2786bf66c3.jpg[/img]"
-anon = "Холодный валенок"
+const webdriver = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+// requires geckodriver https://github.com/mozilla/geckodriver/releases/
+const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+global.wb = require('selenium-webdriver');
+const moment = require('moment')
+
+
+postedid = { // ID комментария
+    "https://catwar.su/blogs": null,
+    "https://catwar.su/sniff": null
+} 
+blogid = { // ID блога
+    "https://catwar.su/blogs": 923180,
+    "https://catwar.su/sniff": 923351
+}
+
+timelimit = {
+    "https://catwar.su/blogs": moment(new Date()).add(3, 'm').toDate(),
+    "https://catwar.su/sniff": moment(new Date()).add(1, 'm').toDate()
+}
+text = {
+    "https://catwar.su/blogs": ".",
+    "https://catwar.su/sniff": "[img]https://i.imgur.com/AP5ei3l.png[/img]"
+}
+anon = { // Псевдоним, под которым нужно отправлять текст. 
+         // Если != "" и запрещено в посте, то выведет ошибку (и перестанет отправлять). Если пустое, то запостит с ником аккаунта
+    "https://catwar.su/blogs": ".",
+    "https://catwar.su/sniff": "."
+}
+
+//
 
 function sleep(ms) {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
     });
 }
-
-const webdriver = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
-// requires geckodriver https://github.com/mozilla/geckodriver/releases/
-
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
-global.wb = require('selenium-webdriver');
 
 async function login() {
     try {
@@ -47,7 +66,7 @@ async function login() {
 }
 
 async function updatepost(kill, url) {
-    await driver.get(`${url}${blogid}`)
+    await driver.get(`${url.replace('blogs', 'blog')}${blogid[url]}`)
     await sleep(3000)
     b = await driver.findElements(By.xpath("//a[@class='comment-delete' and text()='Удалить' and @style!='display: none;']"))
     for (s of b) {
@@ -56,49 +75,72 @@ async function updatepost(kill, url) {
     }
     if (!kill) {
         t = await driver.findElement(By.xpath("//input[@id='comment_anon']"))
-        t.sendKeys(anon)
+        t.sendKeys(anon[url])
         t = await driver.findElement(By.xpath("//textarea[@id='comment']"))
-        t.sendKeys(text)
+        t.sendKeys(text[url])
         await sleep(500)
         t.submit()
         await sleep(1500)
-        b = await driver.findElements(By.xpath("//a[@class='comment-delete' and text()='Удалить' and @style!='display: none;']"))
-        postedid = await b[b.length-1].getAttribute('data-id')
+        try {
+            error = await driver.findElement(By.xpath("//p[@id='error' and @style!='display: none;'][1]"))
+            e = await error.getText()
+            console.log(e)
+            switch (e) {
+            case "Автор запретил оставлять анонимные комментарии.":
+                blogid[url] = null
+            break
+            case "Любим пофлудить?":
+                await sleep(75000)
+            break
+            default:
+                await sleep(30000)
+            break
+            }
+        } catch {
+            b = await driver.findElements(By.xpath("//a[@class='comment-delete' and text()='Удалить' and @style!='display: none;']"))
+            postedid[url] = await b[b.length-1].getAttribute('data-id')
+        }
     } else {
-        postedid = null
-        blogid = null
+        postedid[url] = null
+        blogid[url] = null
+        timelimit[url] = null
     }
 }
 
-var consec_updates = 0
-var url = "https://catwar.su/sniff"
+let consec_updates = 0
+let url
 async function loop() {
     // Checking state
     const cururl = await driver.getCurrentUrl()
     if (cururl == 'https://catwar.su/login') {
         await login()
         await sleep(1000)
-        consec_updates = 25000
-    }
-    else {
-        consec_updates += 1
-        if (consec_updates > 120 || cururl != url) {
-            consec_updates = 0
+        consec_updates = -1
+    } else {
+        consec_updates = (consec_updates + 1) % 60 // Reset page every 60 requests just in case
+        url = (consec_updates % 20) == 0 ? "https://catwar.su/blogs" : "https://catwar.su/sniff"
+        // We check blogs every 20 updates (~14 seconds) since they are not as active as sniffs are
+        if (consec_updates == 0 || cururl != url) {
             await driver.get(url)
             await sleep(2000)
         }
+        console.log(url, consec_updates)
         try {
+            if (blogid[url] && timelimit[url] < new Date()) {
+                console.log(timelimit[url], postedid[url], blogid[url])
+                await updatepost(true, url)
+                return 0
+            }
             m = await driver.findElement(By.id('commentsList'))
             m = await m.findElements(By.xpath('//div[@class=\'comment\']'))
-            if (postedid && blogid) {
+            if (blogid[url]) {
                 var found = false
-                consec_updates = 25000
                 for (var i in m) {
-                    if (i >= postthr) break // Limited to first N comments
+                    if (i >= POSTTHR) break // Limited to first N comments
                     t = m[i]
                     if (!t) break
                     id = await t.getAttribute("data-id")
-                    if (postedid == id) {
+                    if (postedid[url] == id) {
                         found = true
                         break
                     }
@@ -111,7 +153,6 @@ async function loop() {
             console.log(e)
         }
     }
-    setTimeout(loop, 500)
 }
 
 (async function start() {
@@ -122,11 +163,14 @@ async function loop() {
     global.driver = driver
     try {
         await driver.get('https://catwar.su/blogs')
-        loop()
+        while (true) {
+            await loop()
+            await sleep(500)
+        }
     } catch (e) {
         console.log(e)
     } finally {
-        //await driver.quit()
+        await driver.quit()
     }
 })()
 
